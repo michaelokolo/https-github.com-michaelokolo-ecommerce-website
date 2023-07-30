d5c8fcee8839afc4a51619f872d4348b44cc1310
Michael <91018269+michaelokolo@users.noreply.github.com>
2023-07-30 03:10:42 +0000

    added route using react router

---
+++ amazona/front-end/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom"
+
+function ProductScreen(){
+    const params = useParams();
+    const {slug} = params
+    return(
+        <h1>{slug}</h1>
+    )
+}
+
+export default ProductScreen
