5e785c38ed16c0f009ecee1ed7adc35e277b9d6c
Michael <91018269+michaelokolo@users.noreply.github.com>
2023-07-30 01:05:59 +0000

    display products

--- amazona/front-end/src/App.js
+++ amazona/front-end/src/App.js
@@ -8,19 +8,28 @@ function App() {
       </header>
       <main>
         <h1>Featured Products</h1>
+        <div className="products">
         {
           data.products.map((product)=>(
-            <div key={product.slug}>
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name}/>
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
               <p>
                 {product.name}
               </p>
+              </a>
               <p>
-                {product.price}
+                <strong>${product.price}</strong>
               </p>
+              <button>Add to cart</button>
+              </div>
             </div>
           ))
         }
+        </div>
       </main>
     </div>
   );
