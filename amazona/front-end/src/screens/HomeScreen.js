f40fb2c0d6cddf9ebe6f2ef281b37255cfcfb71e
Michael <91018269+michaelokolo@users.noreply.github.com>
2023-08-01 09:05:20 +0000

    using reducer hook to manage state

--- amazona/front-end/src/screens/HomeScreen.js
+++ amazona/front-end/src/screens/HomeScreen.js
@@ -1,15 +1,39 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 //import data from "../data"
 
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return {...state, loading:true}
+    case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading:false}
+    case 'FETCH_FAIL':
+      return {...state, loading:false, error:action.payload}
+    default:
+      return state
+  }
+}
+
 function HomeScreen(){
-    const [products, setProducts] = useState([])
+    const [{loading, error, products}, dispatch] = useReducer(reducer, {
+      products:[],
+      loading:true,
+      error:''
+    })
+    // const [products, setProducts] = useState([])
     useEffect(()=>{
       const fetchData = async () =>{
-        const result = await axios.get('/api/products')
-        console.log(result)
-        setProducts(result.data)
+        dispatch({type: 'FETCH_REQUEST'})
+        try{
+          const result = await axios.get('/api/products')
+          dispatch({type:'FETCH_SUCCESS', payload:result.data})
+        }catch (err){
+          dispatch({type:'FETCH_FAIL', payload:err.message})
+        }
+        
+        // setProducts(result.data)
       }
       fetchData()
       
